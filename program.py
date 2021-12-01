1f1f28f2c39ccbf2d80b8b398141ff98aca3dec9
junyi-s <60148174+junyi-s@users.noreply.github.com>
2021-11-30 22:06:47 -0500

    added

--- program.py
+++ program.py
@@ -2,45 +2,139 @@ import sys
 import string
 import numpy as np
 import math
+from nltk.stem import PorterStemmer
+import numpy as np
 
-def tokenize(line):
-    temp = line.split("ingredients")
-    data = temp[1][3:-3]
-    data = data.replace('"', '')
-    ingredients = data.split(",")
-    return ingredients
-    
-	
-			
+from nltk.tokenize import word_tokenize
+from numpy import dot
+from numpy.linalg import norm
+
+import math
+
+
+def getIngredients(line):
+	temp = line.split("ingredients")
+	data = temp[1][3:-3]
+	data = data.replace('"', '')
+	ingredients = data.split(",")
+	return ingredients
+
+def getCuisine(line):
+	t = line.split("cuisine")
+	t1 = t[1].split(':')
+	t2 = t1[1].split('"')
+	cuisine = t2[1]
+	return cuisine
+
+#idf of query in hw4
+def idf_query(queries_list):
+	temp_idf_query = {}
+	for each_query in queries_list:
+	    query_tokens = word_tokenize(each_query)
+	    num_of_sameword_in_same_query = 0
+	    for word in query_tokens:
+	        if word not in temp_idf_query:
+	            temp_idf_query[word] = 1
+	            num_of_sameword_in_same_query += 1
+	        elif word in temp_idf_query and num_of_sameword_in_same_query == 0:
+	            temp_idf_query[word] += 1
+
+	idf_query_dict = {}
+	for word in temp_idf_query:
+	    idf_query_dict[word] = np.log(9997/temp_idf_query[word])
+
+	return idf_query_dict
+
+def calculate_tf_query(queries_dict):
+	tf_query = {}
+	for current_num in queries_dict:
+	    tf_query[current_num] = {}
+	    query_tokens = word_tokenize(queries_dict[current_num])
+	    for word in query_tokens:
+	        if word not in tf_query[current_num]:
+	            tf_query[current_num][word] = 1
+	        else:
+	            tf_query[current_num][word] += 1
+	return tf_query
+
+
+
+def termCount(terms, freq):
+	for term in terms:
+		if term in freq:
+			freq[term] += 1
+		else:
+			freq[term] = 1
+
+def cosine (vectors_for_abstract):
+	cosine_sim = {}
+	for q_num in vectors_for_abstract:
+		cosine_sim[q_num] = {}
+		q_vector = list(vectors_for_abstract[q_num].values())
+		for a_num in vectors_for_abstract[q_num][a_num].keys():
+			a_vector = list(vectors_for_abstract[q_num][a_num].values())
+			cos_sim = dot(q_vector, a_vector) / (norm(q_vector) * norm(a_vector))
+			if math.isnan(cos_sim):
+				cos_sim = 0
+			cosine_sim[q_num][a_num] = cos_sim
+	return cosine_sim
+
+
 #Main Program
 def main(args):
 	mainfile = open(sys.argv[1], "r")
+	queryfile = mainfile.readlines()
+	recipes = dict()
+	queryTerms = dict()
+	abstractTerms = dict()
+	cuisine = dict()
+	count = 0
+	testing_dict = {}
+	testing_list = []
+	for line in queryfile:
+		data = getIngredients(line)
+		termCount(data, queryTerms)
+		testing_dict[count] = data
+		testing_list.append(data)
+		count += 1
+
+	idf_query_dict = idf_query(testing_list)
+	tf_query_dict = calculate_tf_query(testing_dict)
+
+	tf_idf_query = {}
+	for current_num in tf_query_dict:
+	    tf_idf_query[current_num] = {}
+	    for word in tf_query_dict[current_num]:
+	        tf_idf_query[current_num][word] = tf_query_dict[current_num][word] * idf_query_dict[word]
+
+	print(tf_idf_query)
+
+
+
 
 	#Open Abstracts of Journals
 	readfile = open("/Users/junyi/Documents/NLP/Final Project/NLP_Project/cleaned_corpus/cleanTrain.txt", "r")
 	contents = readfile.readlines()
 	count = 0
-	recipes = {}
+
 
 	for line in contents:
-		data = tokenize(line)
+		data = getIngredients(line)
+		getCuisine(line)
+		termCount(data, abstractTerms)
 		recipes[count] = data
+		cuisine[count] = cuisine
 		count += 1
 
-	queryfile = mainfile.readlines()
-	count = 0
-	testing = {}
-	for line in queryfile:
-		data = tokenize(line)
-		testing[count] = data
-		count += 1
 
-	print(recipes)
-	print(testing)
-		
+
+	# print(termCount)
+	# print(recipes)
+	# print(testing)
 
 
 
 
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
 	main(sys.argv)
---
+++ program1.py
@@ -0,0 +1,72 @@
+import sys
+import string
+import numpy as np
+import math
+
+def getIngredients(line):
+	temp = line.split("ingredients")
+	data = temp[1][3:-3]
+	data = data.replace('"', '')
+	ingredients = data.split(",")
+	return ingredients
+
+def getCuisine(line):
+	t = line.split("cuisine")
+	t1 = t[1].split(':')
+	t2 = t1[1].split('"')
+	cuisine = t2[1]
+	return cuisine
+
+#tf-idf of query in hw4
+def tf_idf_query():
+	temp_idf_query = {}
+
+def termCount(terms, freq):
+	for term in terms:
+		if term in count:
+			count[term] += 1
+		else:
+			count[term] = 1
+
+def cosine (query_vector, abstracts_vector):
+	answers = []
+
+#Main Program
+def main(args):
+	mainfile = open(sys.argv[1], "r")
+
+	#Open Abstracts of Journals
+	readfile = open("/Users/junyi/Documents/NLP/Final Project/NLP_Project/cleaned_corpus/cleanTrain.txt", "r")
+	contents = readfile.readlines()
+	count = 0
+	recipes = dict()
+	freq = {}
+	cuisine = dict()
+
+	for line in contents:
+		data = getIngredients(line)
+		getCuisine(line)
+		# termCount(data, freq)
+		recipes[count] = data
+		cuisine[count] = cuisine
+		count += 1
+
+	queryfile = mainfile.readlines()
+	count = 0
+	testing = {}
+	for line in queryfile:
+		data = getIngredients(line)
+		# termCount(data, freq)
+		testing[count] = data
+		count += 1
+
+	# print(termCount)
+	# print(recipes)
+	# print(testing)
+
+
+
+
+
+if __name__ == '__main__':
+	main(sys.argv)
