e31eb4a2caacde4dcb77bfe051b2508ced3a1725
junyi-s <60148174+junyi-s@users.noreply.github.com>
2021-11-30 23:32:15 -0500

    changes

--- program.py
+++ program.py
@@ -87,7 +87,23 @@ def calculate_tf_abstract(abstract_dict):
 	return tf_abstract
 
 def findCuisine (abstract_index, cuisine_list):
-	cuisine = cuisine_list[abstract_index]
+	answer = cuisine_list[abstract_index]
+	return answer
+
+def sort(cosine_similarity):
+	sorted_output = {}
+	for q_num in cosine_similarity:
+		sorted_output[q_num] = sorted(cosine_similarity[q_num].items(), key=lambda item: item[1], reverse=True)
+	return sorted_output
+
+def results(sorted_output):
+	output = open("output.txt", "w")
+	for q_num in sorted_output:
+		for abs_data in sorted_output[q_num]:
+			if(abs_data[1] == 0):
+				continue
+			output.write(str(q_num) + " " + str(abs_data[0]) + " " + str(abs_data[1]) + "\n")
+	output.close()
 
 def termCount(terms, freq):
 	for term in terms:
@@ -116,12 +132,14 @@ def main(args):
 	recipes = dict()
 	queryTerms = dict()
 	abstractTerms = dict()
-	cuisine = dict()
+	cuisine_abstract = dict()
+	cuisine_query = dict()
 	count = 0
 	testing_dict = {}
 	testing_list = []
 	for line in queryfile:
 		data = getIngredients(line)
+		c = getCuisine(line)
 		termCount(data, queryTerms)
 		#testing_dict[count] = data
 		temp_each_query = ""
@@ -129,6 +147,7 @@ def main(args):
 			temp_each_query += item + " "
 		testing_list.append(temp_each_query)
 		testing_dict[count] = temp_each_query
+		cuisine_query[count] = c
 		count += 1
 
 	idf_query_dict = idf_query(testing_list)
@@ -159,7 +178,7 @@ def main(args):
 		for item in data:
 			temp_each_abstract += item + " "
 		abstract_dict[count] = temp_each_abstract
-		cuisine[count] = c
+		cuisine_abstract[count] = c
 		count += 1
 
 	idf_abstract_dict = calculate_idf_abstract(abstract_dict)
@@ -172,9 +191,31 @@ def main(args):
 			tf_idf_abstract[current_num][word] = tf_abstract_dict[current_num][word] * idf_abstract_dict[word]
 
 	print(tf_idf_abstract)
-	# print(termCount)
-	# print(recipes)
-	# print(testing)
+
+
+	vectors_for_abstract = {} #vectors_for_abstract[Q_num][A_num][word both in Q and A] = [] => vectors(list) for abstract
+#Finding vectors for abstract: Later use for cosine similarity
+	for current_Q_num in tf_idf_query:
+	    current_query_words = tf_idf_query[current_Q_num].keys()
+	    # current_query_vector = []
+	    # for word in current_query_words:
+	    #     current_query_vector = tf_idf_query[current_Q_num][word]
+	    vectors_for_abstract[current_Q_num] = {}
+	    for current_A_num in tf_idf_abstract:
+	        vectors_for_abstract[current_Q_num][current_A_num] = {}
+	        for word in current_query_words:
+	            vectors_for_abstract[current_Q_num][current_A_num][word] = {}
+	            if word in tf_idf_abstract[current_A_num]:
+	                # if tf_idf_abstract[current_A_num][word] <0 :
+	                #     print('here', current_A_num, word)
+	                #     break
+	                vectors_for_abstract[current_Q_num][current_A_num][word] = tf_idf_abstract[current_A_num][word]
+	            else:
+	                vectors_for_abstract[current_Q_num][current_A_num][word] = 0
+
+	cosine = cosine(vectors_for_abstract)
+	sorted_cosine = sort(cosine)
+	answers =
 
 
 
